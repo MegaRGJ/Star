a95c1c04c297ba0a2317b9affd31f72c72689d9e
Ryan Johnson <MegaRGJ@users.noreply.github.com>
2020-02-08 16:27:33 +0000

    Updated the movement to work using a forces
    I also detached the camera as it's causing a ghosting affect

--- Character.cs
+++ Character.cs
@@ -3,38 +3,85 @@ using System;
 
 public class Character : KinematicBody2D
 {
-	public int Speed = 500;
-
+	public int MaxSpeed = 300;
+	public int Acceleration = 10;
+	public int Drag = 10;
+	
 	private Vector2 MovementVelocity = new Vector2();
+	
+	//Make getters later
+	private bool MoveRight = false;
+	private bool MoveLeft = false;
+	private bool MoveUp = false;
+	private bool MoveDown = false;
 	
 	// BeginPlay
 	public override void _Ready()
 	{
 	}
 	
-	
 	public void GetInput()
-	{
-		MovementVelocity = new Vector2();
+	{	
+		MoveRight = Input.IsActionPressed("ui_right");
+		MoveLeft = Input.IsActionPressed("ui_left");
+		MoveDown = Input.IsActionPressed("ui_down");
+		MoveUp = Input.IsActionPressed("ui_up");
 		
-		if(Input.IsActionPressed("ui_right"))
+		if(MoveRight && !MoveLeft)
 		{
-			MovementVelocity.x += Speed;
+			MovementVelocity.x = Math.Min(MovementVelocity.x + Acceleration, MaxSpeed);
+		}
+		else
+		{
+			if (MovementVelocity.x > 0)
+			{
+				MovementVelocity.x = Math.Max(MovementVelocity.x - Drag, 0);
+			}
 		}
 		
-		if(Input.IsActionPressed("ui_left"))
+		if(MoveLeft && !MoveRight)
 		{
-			MovementVelocity.x -= Speed;
+			MovementVelocity.x = Math.Max(MovementVelocity.x - Acceleration, -MaxSpeed);
+		}
+		else
+		{
+			if (MovementVelocity.x < 0)
+			{
+				MovementVelocity.x = Math.Min(MovementVelocity.x + Drag, 0);
+			}
 		}
 		
-		if(Input.IsActionPressed("ui_down"))
+		if(MoveDown && !MoveUp)
 		{
-			MovementVelocity.y += Speed;
+			MovementVelocity.y = Math.Min(MovementVelocity.y + Acceleration, MaxSpeed);
+		}
+		else
+		{
+			if (MovementVelocity.y > 0)
+			{
+				MovementVelocity.y = Math.Max(MovementVelocity.y - Drag, 0);
+			}
 		}
 		
-		if(Input.IsActionPressed("ui_up"))
+		if(MoveUp && !MoveDown)
 		{
-			MovementVelocity.y -= Speed;
+			MovementVelocity.y = Math.Max(MovementVelocity.y - Acceleration, -MaxSpeed);
+		}
+		else
+		{
+			if (MovementVelocity.y < 0)
+			{
+				MovementVelocity.y = Math.Min(MovementVelocity.y + Drag, 0);
+			}
+		}
+		
+		float Mag = (float)Math.Sqrt((MovementVelocity.x * MovementVelocity.x) + (MovementVelocity.y * MovementVelocity.y));
+		if (Mag > MaxSpeed)
+		{
+			if (Mag > 0) // just incase
+			{ 
+				MovementVelocity = new Vector2((MovementVelocity.x / Mag) * MaxSpeed, (MovementVelocity.y / Mag) * MaxSpeed);
+			}
 		}
 	}
 	
